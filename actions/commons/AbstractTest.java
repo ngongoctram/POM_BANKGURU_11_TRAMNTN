63a68c2549d83b56f630f1c862a00f5efb30ab35
tramntn <tramntn@DESKTOP-L754774>
2019-10-07 09:12:41 +0700

    Add POM06_MultiBrowser_Parallel

--- actions/commons/AbstractTest.java
+++ actions/commons/AbstractTest.java
@@ -1,5 +1,32 @@
 package commons;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class AbstractTest {
+	WebDriver driver;
+
+	public WebDriver openMultiBrowser(String browserName) {
+		String projectFolder = System.getProperty("user.dir");
+
+		if (browserName.equals("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectFolder + ".\\resources\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equals("headless")) {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("headless");
+			options.addArguments("window-size=1366x768");
+			driver = new ChromeDriver(options);
+		} else {
+			System.out.println("Please choose browser name in xml file!");
+		}
+
+		return driver;
+
+	}
 
 }
